1ad8cc2fe9189264af3c350b9dcdcdd9222bf0ea
Илья Большаков <gamenest926@gmail.com>
2022-09-22 19:22:11 +0300

    Remove comm

--- WebAPIOrderTracking/Controllers/OrderTrackingController.cs
+++ WebAPIOrderTracking/Controllers/OrderTrackingController.cs
@@ -63,25 +63,24 @@ namespace OrderTrackingWebAPI.Controllers
         [Route("DeleteOrder")]
         public IActionResult DeleteOrder(int id)
         {
-            //try
-            //{
-            //    var orderForRemove = _context.Orders.FirstOrDefault(i => i.Orderid == id);
-            //    if (orderForRemove is Order o) 
-            //    {
-            //        _context.Orders.Remove(o);
-            //        _context.SaveChanges();
-            //        return Ok();
-            //    }
-            //    else
-            //    {
-            //        throw new Exception("Заказа не существует");
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    return Conflict(new { error = ex.Message });
-            //}
-            return Conflict(new { error = "ошибка" });
+            try
+            {
+                var orderForRemove = _context.Orders.FirstOrDefault(i => i.Orderid == id);
+                if (orderForRemove is Order o) 
+                {
+                    _context.Orders.Remove(o);
+                    _context.SaveChanges();
+                    return Ok();
+                }
+                else
+                {
+                    throw new Exception("Заказа не существует");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Conflict(new { error = ex.Message });
+            }
         }
     }
 }
